a6505ab84bdfd91c549df66d15bdb327eafc3b78
rohitkr5850 <rroy854100@gmail.com>
2025-05-25 21:12:06 +0530

    Initial

--- redis_script.js
+++ redis_script.js
@@ -1,6 +1,5 @@
 const redis = require('redis')
 const client = redis.createClient();
-
 client.connect()
     .then(() => console.log('Connected to Redis'))
     .catch(err => console.error('Redis connection error:', err));
